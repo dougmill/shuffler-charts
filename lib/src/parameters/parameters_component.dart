71fe8f7f5f5af312071a63218b799e6ad72880f4
Douglas Miller <millerdouglas@bellsouth.net>
2019-08-10 18:29:23 -0700

    Component for editing all parameters

---
+++ lib/src/parameters/parameters_component.dart
@@ -0,0 +1,52 @@
+import 'package:angular/angular.dart';
+import 'package:angular_forms/angular_forms.dart';
+
+import 'package:built_collection/built_collection.dart';
+import 'package:rxdart/rxdart.dart';
+
+import 'package:shuffler_charts/src/parameters/parameter_component.dart';
+import 'package:shuffler_charts/src/parameters/parameters.dart';
+
+@Component(
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  selector: 'shuffle-parameters',
+  styleUrls: ['parameters_component.css'],
+  templateUrl: 'parameters_component.html',
+  directives: const [coreDirectives, formDirectives, ParameterComponent],
+)
+class ParametersComponent {
+  BuiltList<String> paramKeys;
+  _MapBuilderWrapper<String, Parameter<dynamic>> paramsBuilder;
+  final _output = BehaviorSubject<Parameters>();
+
+  @Input()
+  set params(Parameters params) {
+    if (paramsBuilder == null) {
+      var map = params.asMap;
+      paramKeys = BuiltList.of(map.keys);
+      paramsBuilder = _MapBuilderWrapper(map.toBuilder());
+      paramsBuilder.onSet = () => _output
+          .add(Parameters.fromMap(paramsBuilder.builder.build().asMap()));
+    } else {
+      paramsBuilder.builder = params.asMap.toBuilder();
+    }
+  }
+
+  @Output()
+  Stream<Parameters> get paramsChange => _output.stream;
+}
+
+class _MapBuilderWrapper<K, V> {
+  MapBuilder<K, V> builder;
+
+  void Function() onSet = () {};
+
+  _MapBuilderWrapper(this.builder);
+
+  V operator [](Object key) => builder[key];
+
+  void operator []=(K key, V value) {
+    builder[key] = value;
+    onSet();
+  }
+}
